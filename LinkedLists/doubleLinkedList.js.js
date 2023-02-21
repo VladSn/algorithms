dadd489532780c9c47ca3cedda0db1b7e97ea107
Vladislav <vladakk47@gmail.com>
2023-02-21 13:36:54 +0800

    Add files via upload

---
+++ LinkedLists/doubleLinkedList.js.js
@@ -0,0 +1,62 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+class DoubleLinkedList {
+  constructor(value) {
+    this.head = new Node(value);
+    this.tail = this.head;
+    this.length = 1;
+  }
+
+  append(value) {
+    const newNode = new Node(value);
+    newNode.prev = this.tail;
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.length++;
+    return this;
+  }
+
+  prepend(value) {
+    const newNode = new Node(value);
+    this.head.prev = newNode;
+    newNode.next = this.head;
+    this.head = newNode;
+    this.length++;
+    return this;
+  }
+
+  reverse() {
+    if (!this.head.next) {
+      return this.head;
+    }
+    let first = this.head;
+    this.tail = this.head;
+    let second = first.next;
+    while (second) {
+      const temp = second.next;
+      second.next = first;
+      first = second;
+      second = temp;
+    }
+    this.head.next = null;
+    this.head = first;
+    return this;
+  }
+
+  _print() {
+    console.log(`HEAD: ${this.head.value}`);
+    let currentNode = this.head;
+    while (currentNode.next) {
+      console.log(currentNode.value);
+      currentNode = currentNode.next;
+    }
+    console.log(currentNode.value);
+    console.log(`TAIL: ${this.tail.value}`);
+  }
+}
